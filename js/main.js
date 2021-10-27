de530e18e58238fa13354f34a579d38c9222926f
selenali07 <selenali07@users.noreply.github.com>
2021-10-27 08:54:22 +0000

    deploy: 96a8bc89dbb8f680588df5636ea6a38f47393334

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
+const hamburgerIcon = document.querySelector(".hamburger-menu");
+const overlay = document.querySelector(".overlay");
 
+hamburgerIcon.addEventListener("click", openMenu);
+
+function openMenu() {
+  hamburgerIcon.classList.toggle("open");
+  overlay.classList.toggle("open");
+}
