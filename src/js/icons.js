9856fc059064f1679c5f14ffc485d4fa3f9284d2
Selena Li <selenali@live.unc.edu>
2021-10-28 16:22:56 -0400

    removed log statements

--- src/js/icons.js
+++ src/js/icons.js
@@ -6,9 +6,7 @@ $(document).scroll(function () {
   scroll_pos = $(this).scrollTop();
   if (scroll_pos > vh - 90) {
     $(".icon-bar").addClass("unhide");
-    console.log(1);
   } else {
     $(".icon-bar").removeClass("unhide");
-    console.log(0);
   }
 });
